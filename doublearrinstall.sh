52139cb17645bc3c46e448194bc6529be31453af
ComputerByte <46009342+ComputerByte@users.noreply.github.com>
2022-02-22 19:52:25 -0700

    Update doublearrinstall.sh
    Use reverse proxy as shown on https://wiki.servarr.com/radarr/installation

--- doublearrinstall.sh
+++ doublearrinstall.sh
@@ -146,13 +146,26 @@ SERV
     cat >/etc/nginx/apps/radarr4k.conf <<-NGX
 location /radarr4k {
   proxy_pass        http://127.0.0.1:9000/radarr4k;
-  proxy_set_header Host \$proxy_host;
+  proxy_set_header Host \$host;
   proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
   proxy_set_header X-Forwarded-Proto \$scheme;
   proxy_redirect off;
   auth_basic "What's the password?";
   auth_basic_user_file /etc/htpasswd.d/htpasswd.${user};
 
+  proxy_http_version 1.1;
+  proxy_set_header Upgrade \$http_upgrade;
+  proxy_set_header Connection \$http_connection;
+}
+
+location  /radarr/api {
+  proxy_pass        http://127.0.0.1:9000/radarr/api;
+  proxy_set_header Host \$host;
+  proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+  proxy_set_header X-Forwarded-Host \$host;
+  proxy_set_header X-Forwarded-Proto \$scheme;
+  proxy_redirect off;
+  auth_basic off;
   proxy_http_version 1.1;
   proxy_set_header Upgrade \$http_upgrade;
   proxy_set_header Connection \$http_connection;
